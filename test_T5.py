7fb0cd3e6430684d2752e138f12e67a51489d9f2
Yuning30 <1987150762@qq.com>
2023-04-21 01:43:06 +0000

    add cuda to test T5

--- test_T5.py
+++ test_T5.py
@@ -4,19 +4,22 @@ import torch
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 from transformers import AdamW, get_cosine_schedule_with_warmup
 
+device = 'cuda'
 path = "entailment_bank/data/public_dataset/entailment_trees_emnlp2021_data_v2/dataset/task_1/"
 
 tokenizer = T5Tokenizer.from_pretrained("t5-large")
 
-model = T5ForConditionalGeneration.from_pretrained("./T5_large_epoch_2")
+model = T5ForConditionalGeneration.from_pretrained("./saved_model/T5_large_epoch_2")
+model = model.to(device)
 
 dataset_validation = preprocess.entailment_bank_dataset(path + "dev.jsonl")
 
-for i in range(0, 100):
+for i in range(0, 15):
     x, y = dataset_validation[i]
     print(x)
     input_ids = tokenizer(x, return_tensors="pt").input_ids
-    outputs = model.generate(input_ids, num_beams=20, max_length=128, num_return_sequences=10, do_sample=True)
+    input_ids = input_ids.to(device)
+    outputs = model.generate(input_ids, num_beams=4, max_length=128, num_return_sequences=3)
     # import pdb
     # pdb.set_trace()
     print("ground truth\t", y)
